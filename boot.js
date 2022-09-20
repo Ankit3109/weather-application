83158fb677cc819bb4d8893a2e6fb86db84163cd
Neetika Suroop <suroopneetika123@gmail.com>
2022-09-20 13:12:31 +0530

    weather app

---
+++ boot.js
@@ -0,0 +1,8 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.display="block";
+    document.getElementById('slider').style.opacity=".5";
+}
+function closeCoupon(){
+    document.getElementById('coupon').style.display="none";
+    document.getElementById('slider').style.opacity="1";
+}
